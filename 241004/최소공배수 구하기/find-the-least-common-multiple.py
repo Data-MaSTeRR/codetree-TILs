9605bd38feee0b1900986eab3fd5d70fa1d1dc75
HW. S <163520887+Data-MaSTeRR@users.noreply.github.com>
2024-10-04 21:41:59 +0900

    Title: 최소공배수 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241004/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,39 @@
+def baesu(num):
+
+    l = []
+    cnt = 1
+    mul_num = num
+    while (mul_num <= 100):
+        
+        l.append(mul_num)
+        cnt += 1
+        mul_num = num * cnt
+
+    return l
+
+
+def min_baesu(n_list, m_list):
+
+    flag = False
+    for n in n_list:
+        for m in m_list:
+            if n == m:
+                min_value = m
+                flag = True
+                break
+        
+        if flag:
+            break
+
+    print(min_value)
+
+    return min_value
+
+
+
+
+n, m = map(int, input().split())
+n_list = baesu(n)
+m_list = baesu(m)
+
+min_baesu(n_list, m_list)
