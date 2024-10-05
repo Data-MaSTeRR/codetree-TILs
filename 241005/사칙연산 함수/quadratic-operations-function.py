1cb9a9140acf193502f24280896ac51eb04d2b8d
HW. S <163520887+Data-MaSTeRR@users.noreply.github.com>
2024-10-05 14:10:19 +0900

    Title: 사칙연산 함수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241005/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,30 @@
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+def plus(a, c):
+    return a + c
+
+def minus(a, c):
+    return a - c
+
+def multiply(a, c):
+    return a * c
+
+def divide(a, c):
+    return int(a / c)
+
+if o == "+":
+    print(f"{a} {o} {c} = {plus(a,c)}")
+
+elif o == "-":
+    print(f"{a} {o} {c} = {minuse(a,c)}")
+
+elif o == "*":
+    print(f"{a} {o} {c} = multiply{(a,c)}")
+
+elif o == "/":
+    print(f"{a} {o} {c} = {divide(a,c)}")
+
+else:
+    print("False")
