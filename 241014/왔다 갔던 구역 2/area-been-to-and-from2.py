d19d873d1b333328fd1882f3e3eec3be0976b353
HW. S <163520887+Data-MaSTeRR@users.noreply.github.com>
2024-10-14 15:47:15 +0900

    Title: 왔다 갔던 구역 2, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 241014/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 241014/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -20,8 +20,10 @@ for nd_list in matrix:
     n_matrix.append([section_left, section_right])
 
 
+
 count_list = [ 0 for _ in range(2001)]
 for n_list in n_matrix:
+    # 양 끝을 신경쓰지 않는이유??? -> 2번 이상 겹치는 양 끝은 구간에서 -1 고려하는 것과 같음
     for i in range(n_list[0] + 1000, n_list[1] + 1000):
         count_list[i - 1] += 1
 
