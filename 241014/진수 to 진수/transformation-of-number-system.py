ea7a2926cf21e5292002db1c638938286f2a1125
HW. S <163520887+Data-MaSTeRR@users.noreply.github.com>
2024-10-14 12:20:08 +0900

    Title: 진수 to 진수, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241014/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,38 @@
+a, b = map(int, input().split())
+n = int(input())
+
+# a진수인 n, 10진수 변환
+def _10Digit(a, n):
+
+    n_list = [ int(i) for i in list(str(n)) ]
+    tendigit = 0
+    temp = 0
+
+    for n in n_list[::-1]:
+        
+        tendigit += n * (a ** temp)
+        temp += 1
+
+    return tendigit
+
+# 10진수 -> b진수로 변환
+def _bDigit(tendigit, b):
+
+    bdigit_list = []
+
+    while True:
+
+        if tendigit < b:
+            bdigit_list.append(tendigit)
+            break
+        
+        bdigit_list.append(tendigit % b)
+        tendigit //= b
+
+    bdigit = int(''.join(map(str, bdigit_list)))
+
+    return bdigit
+
+tendigit = _10Digit(a, n)
+
+print(_bDigit(tendigit, b))
