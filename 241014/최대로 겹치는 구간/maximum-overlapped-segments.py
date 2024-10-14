8e236075dac0a006ae3f44d82a00c26aa0f86ec3
HW. S <163520887+Data-MaSTeRR@users.noreply.github.com>
2024-10-14 14:38:08 +0900

    Title: 최대로 겹치는 구간, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241014/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 241014/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -10,9 +10,7 @@ for n_list in matrix:
     for i in range(n_list[0] + 100, n_list[1] + 100 + 1):
         num_list.append(i)
 
-    for n in n_list:
-        if n > max_num:
-            max_num = n 
+print(num_list)
 
 # 원소마다 값을 카운트
 count_list = [ 0 for _ in range(201) ]
@@ -20,4 +18,5 @@ for num in num_list:
 
     count_list[num] += 1
 
+print(count_list)
 print(max(count_list))
