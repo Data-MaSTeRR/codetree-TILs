2fb20b01aae1aaf61ae92c8fb2e59272f9d1b560
HW. S <163520887+Data-MaSTeRR@users.noreply.github.com>
2024-10-18 13:43:57 +0900

    Title: 연속되는 수 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241018/연속되는 수 2/continuous-number2.py
+++ 241018/연속되는 수 2/continuous-number2.py
@@ -1,12 +1,21 @@
 N = int(input())
 n_list = [ int(input()) for _ in range(N) ]
 
-cnt = 0
-for idx in range(N):
-    if idx == 0:
-        cnt += 1
-        continue
-    if n_list[idx] != n_list[idx - 1]:
-        cnt += 1
 
-print(cnt)
+temp, max_cnt = 0, 0
+for idx in range(N):
+    
+    if idx == 0:
+        temp += 1
+        continue
+
+    if n_list[idx] != n_list[idx - 1]:
+        
+        if max_cnt < temp:
+            max_cnt = temp
+        
+        temp = 0
+
+    temp += 1
+
+print(max_cnt)
