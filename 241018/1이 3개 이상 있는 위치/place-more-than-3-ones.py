6e65035d21d969b6a2cdb7dc04b706771586d8b1
HW. S <163520887+Data-MaSTeRR@users.noreply.github.com>
2024-10-18 22:59:06 +0900

    Title: 1이 3개 이상 있는 위치, Time: 167ms, Memory: 25MB, Status: Passed - CodeTree

--- 241018/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 241018/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -7,16 +7,23 @@ dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
 dir_list = [0, 1, 2, 3]
 
 def in_range(x, y):
-    if x >= 0 and y >= 0 and x < 5 and y < 5:
+    if x >= 0 and y >= 0 and x < n and y < n:
         return True
 
 cnt = 0
-for y in range(n):
-    for x in range(n):
+for x in range(n):
+    for y in range(n):
+  
+        overcnt = 0
         for dir_num in dir_list:
-            x += dx[dir_num]
-            y += dy[dir_num]
-            if matrix[x][y] == 1 and in_range(x, y):
-                cnt += 1
+            tempx, tempy = x, y
+            tempx += dx[dir_num]
+            tempy += dy[dir_num]
+            if in_range(tempx, tempy) == True and matrix[tempx][tempy] == 1:
+                overcnt += 1
+           
+        
+        if overcnt >= 3:
+            cnt += 1
 
 print(cnt)
