c8e9c10a89bbaa7c89ca14378a79b89e367f1a96
HW. S <163520887+Data-MaSTeRR@users.noreply.github.com>
2024-10-30 21:49:03 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241030/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -0,0 +1,34 @@
+n, m = map(int, input().split())
+matrix = [ [0] * m for _ in range(m) ]
+
+## 남북 부호유의
+# 동, 남, 서, 북
+dxs = [1, 0, -1, 0]
+dys = [0, 1, 0, -1]
+
+y, x, move_dir = 0, 0, 0
+
+def in_range(y, x):
+    return y >= 0 and x >= 0 and y < n and x < m
+
+matrix[0][0] = chr(65) 
+
+# chr(65) = A / ord(A) = 65
+for i in range(66, 64 + (n*m + 1)):
+
+    ny, nx = y + dys[move_dir], x + dxs[move_dir]
+    
+    if in_range(ny, nx) and matrix[ny][nx] == 0:
+        matrix[ny][nx] = chr(i)
+    
+    else:
+        move_dir = (move_dir + 1) % 4
+        ny, nx = y + dys[move_dir], x + dxs[move_dir]
+        matrix[ny][nx] = chr(i)
+    
+    y, x = ny, nx
+
+for y in range(n):
+    for x in range(m):
+        print(matrix[y][x], end = ' ')
+    print()
