a7e63f0f6837f23020b9875ffb2c72954481197a
HW. S <163520887+Data-MaSTeRR@users.noreply.github.com>
2024-10-30 23:41:58 +0900

    Title: 일렬로 서있는 소 2, Time: 133ms, Memory: 25MB, Status: Passed - CodeTree

--- 241030/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 241030/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -6,6 +6,7 @@ for i in range(N):
     for j in range(i+1, N):
         for k in range(j+1, N):
             
+            # a_list[i] <= a_list[j] and a_list[j] <= a_list[k]
             if a_list[i] <= a_list[j] <= a_list[k]:
                 cnt += 1
 
