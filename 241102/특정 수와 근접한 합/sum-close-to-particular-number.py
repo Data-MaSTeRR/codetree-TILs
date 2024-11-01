4a3ff66f9ca74876948b86f196dd409fee556585
HW. S <163520887+Data-MaSTeRR@users.noreply.github.com>
2024-11-02 01:36:42 +0900

    Title: 특정 수와 근접한 합, Time: 134ms, Memory: 19MB, Status: Passed - CodeTree

--- 241102/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 241102/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -9,8 +9,9 @@ for i in range(N-1):
 
         copy_list = copy.deepcopy(n_list)
         copy_list.pop(i)
+        #print(copy_list)
         copy_list.pop(j-1)
-
-        min_n = min(abs(sum(copy_list) - 20), min_n)
+        #print(copy_list)
+        min_n = min(abs(sum(copy_list) - S), min_n)
     
 print(min_n)
