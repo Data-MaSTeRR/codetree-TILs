9b45acc138b22b06c94f8345b0ad58f8324c4e16
HW. S <163520887+Data-MaSTeRR@users.noreply.github.com>
2024-11-02 01:17:24 +0900

    Title: 씨 오 더블유 2, Time: 54ms, Memory: 16MB, Status: Passed - CodeTree

--- 241102/씨 오 더블유 2/c-o-w-2.py
+++ 241102/씨 오 더블유 2/c-o-w-2.py
@@ -1,8 +1,11 @@
 N = int(input())
-st = input()
+s = input()
 
-c = st.count('C')
-o = st.count('O')
-w = st.count('W')
-
-print(c * o * w)
+cnt = 0
+for c in range(N-2):
+    for o in range(c+1, N-1):
+        for w in range(o+1, N):
+            if s[c] == 'C' and s[o] == 'O' and s[w] == 'W':
+                cnt += 1
+            
+print(cnt)
