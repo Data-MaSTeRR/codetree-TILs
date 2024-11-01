219c6137cc7bd78f752d1f31e8d4f39e54559ab4
HW. S <163520887+Data-MaSTeRR@users.noreply.github.com>
2024-11-01 16:53:41 +0900

    Title: 이상한 진수 2, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241101/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,19 @@
+a = input()
+a_list = list(a)
+
+
+for idx in range(len(a)):
+    if a_list[idx] == '0':
+        a_list[idx] = '1'
+        break
+
+a = ''.join(a_list)
+int_a = int(a)
+
+_10int  = 0
+for i in range(len(a)):
+
+    _10int += int_a % 10 * (2 ** i)
+    int_a //= 10
+
+print(_10int)
